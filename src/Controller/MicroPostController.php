ff09ebbb7f10f2b8f18893078c8bfd50c26f5f54
MarosAware <marosdevone@gmail.com>
2018-09-23 14:33:32 +0200

    Entity migration and template created. Changed bootstrap version.

---
+++ src/Controller/MicroPostController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Controller;
+
+
+use App\Repository\MicroPostRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * Class MicroPostController
+ * @package App\Controller
+ * @Route("/micro-post")
+ */
+class MicroPostController extends AbstractController
+{
+
+    /**
+     * @var MicroPostRepository
+     */
+    private $microPostRepository;
+
+    public function __construct(MicroPostRepository $microPostRepository)
+    {
+
+        $this->microPostRepository = $microPostRepository;
+    }
+
+    /**
+     * @Route("/", name="micro_post_index")
+     */
+    public function index()
+    {
+        $posts = $this->microPostRepository->findAll();
+
+        return $this->render('micro-post/index.html.twig', compact($posts));
+    }
+}
