529281cf9052c4379739702d6f722533b5a765af
MarosAware <marosdevone@gmail.com>
2018-09-26 23:30:48 +0200

    Created add action and form class.

--- src/Controller/MicroPostController.php
+++ src/Controller/MicroPostController.php
@@ -3,8 +3,13 @@
 namespace App\Controller;
 
 
+use App\Entity\MicroPost;
 use App\Repository\MicroPostRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\FormFactoryInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 /**
@@ -19,11 +24,27 @@ class MicroPostController extends AbstractController
      * @var MicroPostRepository
      */
     private $microPostRepository;
+    /**
+     * @var \Twig_Environment
+     */
+    private $twig;
+    /**
+     * @var FormFactoryInterface
+     */
+    private $formFactory;
+    /**
+     * @var EntityManagerInterface
+     */
+    private $manager;
 
-    public function __construct(MicroPostRepository $microPostRepository)
-    {
-
+    public function __construct(
+        MicroPostRepository $microPostRepository, \Twig_Environment $twig,
+        FormFactoryInterface $formFactory, EntityManagerInterface $manager
+    ) {
         $this->microPostRepository = $microPostRepository;
+        $this->twig = $twig;
+        $this->formFactory = $formFactory;
+        $this->manager = $manager;
     }
 
     /**
@@ -34,5 +55,32 @@ class MicroPostController extends AbstractController
         $posts = $this->microPostRepository->findAll();
 
         return $this->render('micro-post/index.html.twig', ['posts' => $posts]);
+    }
+
+    /**
+     * @Route("/add", name="micro_post_add")
+     */
+    public function add(Request $request)
+    {
+        $microPost = new MicroPost();
+        $microPost->setTime(new \DateTime());
+
+        // Other method of create form by using form factory interface
+        // $form = $this->formFactory->create('App\Form\MicroPostType', $microPost);
+        $form = $this->createForm('App\Form\MicroPostType', $microPost);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($microPost);
+            $em->flush();
+
+            return $this->redirectToRoute('micro_post_index');
+        }
+        return new Response(
+            $this->twig->render('micro-post/add.html.twig', [
+                'form' => $form->createView()
+            ])
+        );
     }
 }
